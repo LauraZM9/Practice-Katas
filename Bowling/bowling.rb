f1f524ca33b1decfae608c086660f9a6a4bcbbb1
Laura Zaharia <zaharialaura9@gmail.com>
2022-02-09 17:05:45 +0000

    Add score calculation for strike

--- Bowling/bowling.rb
+++ Bowling/bowling.rb
@@ -1,12 +1,30 @@
 class Bowling
-    def get_score_frame(frame)
-        first_roll = frame[0]  
-        second_roll = frame[1]         # '-'.to_i -> 0???
-        if second_roll == "/" 
-            score = 10
-        else
-            score = first_roll.to_i + second_roll.to_i
+    def get_total_score(frames)
+        frames_list = frames.split(" ")
+        score = 0
+        frames_list.each do |frame|
+            if frame == "X" && frames_list.index(frame) < frames_list.length - 1
+                next_frame = frames_list[frames_list.index(frame) + 1]
+                score += 10 + get_score_frame(next_frame)
+            else
+                score += get_score_frame(frame)
+            end
         end
         score
+    end
+
+    def get_score_frame(frame)
+        if frame == "X"
+            score = 10
+        else
+            first_roll = frame[0]  
+            second_roll = frame[1]         # Anything else besides digit will be: '-'.to_i -> 0
+            if second_roll == "/" 
+                score = 10
+            else
+                score = first_roll.to_i + second_roll.to_i
+            end
+        end
+        score 
     end
 end 
--- Bowling/spec/bowling_spec.rb
+++ Bowling/spec/bowling_spec.rb
@@ -6,7 +6,7 @@ describe Bowling do
         bowling = described_class.new
 
         # Act
-        result = bowling.get_score_frame("45")
+        result = bowling.get_total_score("45")
 
         # Assert
         expect(result).to eq(9)
@@ -17,7 +17,7 @@ describe Bowling do
         bowling = described_class.new
 
         # Act
-        result = bowling.get_score_frame("32")
+        result = bowling.get_total_score("32")
 
         # Assert
         expect(result).to eq(5)
@@ -28,18 +28,7 @@ describe Bowling do
         bowling = described_class.new
 
         # Act
-        result = bowling.get_score_frame("9-")   
-
-        # Assert
-        expect(result).to eq(9)
-    end
-
-    it "adds a roll and a miss" do
-        # Arrange
-        bowling = described_class.new
-
-        # Act
-        result = bowling.get_score_frame("9-")      
+        result = bowling.get_total_score("9-")   
 
         # Assert
         expect(result).to eq(9)
@@ -50,9 +39,54 @@ describe Bowling do
         bowling = described_class.new
 
         # Act
-        result = bowling.get_score_frame("5/")      
+        result = bowling.get_total_score("5/")      
 
         # Assert
         expect(result).to eq(10)
     end
+
+    it "adds a strike" do
+        # Arrange
+        bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("X")      
+
+        # Assert
+        expect(result).to eq(10)
+    end
+
+    it "adds two frames with first one being a number" do
+        # Arrange
+        bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("45 32")      
+
+        # Assert
+        expect(result).to eq(14)
+    end
+
+    it "adds two frames with first one strike and second number" do
+        # Arrange
+        bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("X 45")      
+
+        # Assert
+        expect(result).to eq(28)
+    end
 end 
+
+# "45 32"
+# "45 9-"
+# "45 X"
+# "45 5/"
+# "X X" *
+# "X 45"
+# "X 5/" *
+# "X 9-" *
+# "5/ 9-" *
+# "5/ 5/" *
+# "9- 9-"
