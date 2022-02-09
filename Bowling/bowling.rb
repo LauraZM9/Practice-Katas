0fdb4d48253886b7e877a8fd8c76b4a726eba50d
Laura Zaharia <zaharialaura9@gmail.com>
2022-02-09 14:41:53 +0000

    Add first passed test

--- Bowling/bowling.rb
+++ Bowling/bowling.rb
@@ -1,5 +1,5 @@
 class Bowling
     def do_something(thing)
-        "something"
+        9
     end
 end
--- Bowling/spec/bowling_spec.rb
+++ Bowling/spec/bowling_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../bowling'
 
 describe Bowling do
-    it "does something" do
+    it "adds two rolls together" do
         # Arrange
         bowling = described_class.new
 
         # Act
-        result = bowling.do_something("something")
+        result = bowling.do_something("45")
 
         # Assert
-        expect(result).to eq("something")
+        expect(result).to eq(9)
     end
 end 
