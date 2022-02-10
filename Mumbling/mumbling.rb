23f7f29f007f92b529a2441be3c5147e6f532bec
LauraZ <zaharialaura9@gmail.com>
2022-02-10 23:30:40 +0000

    Add solution that passes all tests for mumbling

--- Mumbling/mumbling.rb
+++ Mumbling/mumbling.rb
@@ -1,5 +1,10 @@
 class Mumbling
-    def mumble_letters(x)
-        "something"
+    def mumble_letters(letters)
+        num_chars = letters.length
+        one_mumble = Array.new
+        (0...num_chars).each do |index|
+            one_mumble << letters[index].upcase + (letters[index]*index).downcase
+        end
+        one_mumble.join("-")
     end
 end
--- Mumbling/spec/mumbling_spec.rb
+++ Mumbling/spec/mumbling_spec.rb
@@ -1,10 +1,35 @@
 require_relative '../mumbling'
 
 describe Mumbling do 
-    mumble = Mumbling.new
-    it "handles one letter strings" do
-        mumbled_letters = mumble.mumble_letters("a")
+    mumbling = Mumbling.new
 
-        expect(mumbled_letters).to eq("something")
+    it "with one letter string" do
+        mumbled_letters = mumbling.mumble_letters("a")
+        expect(mumbled_letters).to eq("A")
+    end
+
+    it "with two letters string" do
+        mumbled_letters = mumbling.mumble_letters("ab")
+        expect(mumbled_letters).to eq("A-Bb")
+    end
+
+    it "with 3 letters string and some capitals" do
+        mumbled_letters = mumbling.mumble_letters("abC")
+        expect(mumbled_letters).to eq("A-Bb-Ccc")
+    end
+
+    it "with 4 letters string and some capitals" do
+        mumbled_letters = mumbling.mumble_letters("aBCd")
+        expect(mumbled_letters).to eq("A-Bb-Ccc-Dddd")
+    end
+
+    it "with 6 letters string all capitals" do
+        mumbled_letters = mumbling.mumble_letters("QWERTY")
+        expect(mumbled_letters).to eq("Q-Ww-Eee-Rrrr-Ttttt-Yyyyyy")
+    end
+
+    it "with 7 letters string and some capitals" do
+        mumbled_letters = mumbling.mumble_letters("RqaEzty")
+        expect(mumbled_letters).to eq("R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
     end
 end
